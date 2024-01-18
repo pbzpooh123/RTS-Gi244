e8f910f47ea27daabef186320d08d2f2312a7ebe
PBZpooh123 <endlessstar4@gmail.com>
2024-01-18 10:05:23 +0700

    Zoom

--- My project/Assets/Scipt/CameraController.cs
+++ My project/Assets/Scipt/CameraController.cs
@@ -10,6 +10,11 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform coner2;
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
+    [Header("Zoom")] [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomModifier;
+    [SerializeField] private float minZoomDist;
+    [SerializeField] private float maxZoomDist;
+    [SerializeField] private float dist;
 
     public static CameraController instance;
 
@@ -22,6 +27,9 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         moveSpeed = 50;
+        zoomSpeed = 25;
+        minZoomDist = 15;
+        maxZoomDist = 50;
     }
 
     private void MoveByKB()
@@ -32,9 +40,46 @@ public class CameraController : MonoBehaviour
         Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
 
         transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position = Clamp(coner1.position, coner2.position);
+    }
+
+    private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x, lowerLeft.x, topRight.x), transform.position.y,
+            Mathf.Clamp(transform.position.z, lowerLeft.z, topRight.z));
+
+        return pos;
+    }
+
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+        {
+            zoomModifier = 0.01f;
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            zoomModifier = -0.01f;
+        }
+
+        dist = Vector3.Distance(transform.position, cam.transform.position);
+        if (dist < minZoomDist && zoomModifier > 0f)
+        {
+            return; // too close!!!!!!
+        }
+        else if (dist > maxZoomDist && zoomModifier < 0f)
+        {
+            return; // too far !!!!!!
+        }
+
+        {
+            cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
+        }
     }
     void Update()
     {
         MoveByKB();
+        Zoom();
     }
 }
