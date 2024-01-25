134a7de70eed0abb06ac95369dea8f65a1a775e0
PBZpooh123 <endlessstar4@gmail.com>
2024-01-25 10:36:18 +0700

    panel test anim

---
+++ My project/Assets/Scipt/Animation/UnitAnimation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    private Units unit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        unit = GetComponent<Units>();
+    }
+
+    private void ChooseAnimation(Units u)
+    {
+        anim.SetBool("IsIdle", false);
+        anim.SetBool("IsMove", false);
+        anim.SetBool("IsAttack", false);
+        switch (u.State)
+        {
+            case UnitState.Idle:
+                anim.SetBool("IsIdle", true);
+                break;
+            case UnitState.Move:
+                anim.SetBool("IsMove", true);
+                break;
+            case UnitState.Attack:
+                anim.SetBool("IsAttack", true);
+                break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChooseAnimation(unit);
+    }
+}
