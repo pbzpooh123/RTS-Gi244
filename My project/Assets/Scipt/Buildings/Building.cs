c29eeb3eb8ff61901a6adc7878f71f61d9b5f05e
pbzpooh123 <endlessstar4@gmail.com>
2024-04-23 19:29:39 +0700

    Final

--- My project/Assets/Scipt/Buildings/Building.cs
+++ My project/Assets/Scipt/Buildings/Building.cs
@@ -52,7 +52,7 @@ public class Building : Structure
     }
     private float timer = 0f; //Constructing timer
     public float Timer { get { return timer; } set { timer = value; } }
-    private float waitTime = 0.5f; //How fast it will be construct, higher is longer
+    private float waitTime = 0.15f; //How fast it will be construct, higher is longer
     public float WaitTime { get { return waitTime; } set { waitTime = value; } }
 
     public void ToCreateUnit(int i)
